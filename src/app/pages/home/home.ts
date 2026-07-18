cdcbf58c1aa86a8074b4f4fe6e9bd50c4336ba33
Casimiro <casimiroarocahenares@gmail.com>
2026-07-18 13:45:56 +0200

    refactor: extract and improve contact component

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -5,10 +5,11 @@ import { Hero } from '../../components/hero/hero';
 import { FeaturedProject } from '../../components/featured-project/featured-project';
 import { About } from '../../components/about/about';
 import { Skills } from '../../components/skills/skills';
+import { Contact } from "../../components/contact/contact";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero, FeaturedProject, About, Skills],
+  imports: [Navbar, Hero, FeaturedProject, About, Skills, Contact],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
