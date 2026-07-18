33232d8d39c88f2105cd56fbd889f8baedfcbb87
Casimiro <casimiroarocahenares@gmail.com>
2026-07-18 13:29:09 +0200

    refactor: improve about componen

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -3,10 +3,11 @@ import { Skill } from '../../models/skill.model';
 import { Navbar } from '../../components/navbar/navbar';
 import { Hero } from '../../components/hero/hero';
 import { FeaturedProject } from '../../components/featured-project/featured-project';
+import { About } from '../../components/about/about';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero, FeaturedProject],
+  imports: [Navbar, Hero, FeaturedProject, About],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
