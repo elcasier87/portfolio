9f58f21aa8acd721992f48a88bbff7e6c603f24e
Casimiro <casimiroarocahenares@gmail.com>
2026-07-18 13:48:34 +0200

    refactor: complete home component modularization

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -6,10 +6,11 @@ import { FeaturedProject } from '../../components/featured-project/featured-proj
 import { About } from '../../components/about/about';
 import { Skills } from '../../components/skills/skills';
 import { Contact } from "../../components/contact/contact";
+import { Footer } from "../../components/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar, Hero, FeaturedProject, About, Skills, Contact],
+  imports: [Navbar, Hero, FeaturedProject, About, Skills, Contact, Footer],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
